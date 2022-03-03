b46b53ad4dc6d6bbec9c1c468031cad5ca870971
André Cardoso <65349469+andrecardoso19@users.noreply.github.com>
2022-03-03 16:44:37 -0300

    Add files via upload

---
+++ ViewCode_Estudo/Clientes+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Clientes+CoreDataClass.swift
+//  teste_login
+//
+//  Created by user211178 on 2/4/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Clientes: NSManagedObject {
+
+}
