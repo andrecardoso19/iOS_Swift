340e5d9bbf91b28fb10b64ae02dd58748fc9415e
André Cardoso <65349469+andrecardoso19@users.noreply.github.com>
2023-01-26 09:16:34 -0300

    Add files via upload

---
+++ PokemonLister/PokemonListerTests/PokemonListerTests.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonListerTests.swift
+//  PokemonListerTests
+//
+//  Created by andre.aragao on 26/01/23.
+//
+
+import XCTest
+@testable import PokemonLister
+
+class PokemonListerTests: XCTestCase {
+
+}
